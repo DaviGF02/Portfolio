8aab63044d589c66a0c5d087ca5b45947f2b105f
Davi de Godoy Ferreira <davigf2002@gmail.com>
2023-02-09 16:58:17 -0300

    calculadora concluida

--- Calculadora_BoraCodar5_rocketSeat/scripts.js
+++ Calculadora_BoraCodar5_rocketSeat/scripts.js
@@ -12,24 +12,34 @@ const pendingOperator = () => operator !== undefined;
 
 const calculate = () => {
     if (pendingOperator()) {
-        const currentNumber = parseFloat(display.textContent);
-        let newNumber = true;
-        if (operator == "+") {
-            console.log(lastNumber);
-            console.log(currentNumber);
-            updateDisplay(lastNumber + currentNumber);
+        const currentNumber = parseFloat(display.textContent.replace(',', '.'));
+        newNumber = true;
+        switch (operator) {
+            case '+':
+                updateDisplay(lastNumber + currentNumber);
+                break;
+            case '-':
+                updateDisplay(lastNumber - currentNumber);
+                break;
+            case 'X':
+                updateDisplay(lastNumber * currentNumber);
+                break;
+            case '/':
+                updateDisplay(lastNumber / currentNumber);
+                break;
+            case '%':
+                updateDisplay(lastNumber % currentNumber);
+                break;
         }
-    } else {
-        
     }
 }
 
 const updateDisplay = (displayText) => {
     if (newNumber) {
-        display.textContent = displayText;
+        display.textContent = displayText.toLocaleString('BR');
         newNumber = false;
     } else {
-        display.textContent += displayText;
+        display.textContent += displayText.toLocaleString('BR');
     }
 }
 
@@ -37,14 +47,87 @@ const insertNumber = (eventAction) => updateDisplay(eventAction.target.textConte
 numeros.forEach(numero => numero.addEventListener('click', insertNumber));
 
 const selectOperator = (eventAction) => {
-    if (!lastNumber) {
+    if (!newNumber) {
         calculate();
         newNumber = true;
         operator = eventAction.target.textContent;
-        lastNumber = parseFloat(display.textContent);
-        console.log(operator);
-    } else {
-        
+        lastNumber = parseFloat(display.textContent.replace(',', '.'));
     }
 }
 operators.forEach(numeros => numeros.addEventListener('click', selectOperator));
+
+const showResult = () => {
+    calculate();
+    operator = undefined;
+}
+document.getElementById('equal').addEventListener('click', showResult);
+
+const cleanDisplay = () => {
+    display.textContent = '';
+}
+document.getElementById('cleanDisplay').addEventListener('click', cleanDisplay);
+
+const cleanCalc = () => {
+    cleanDisplay();
+    operator = undefined;
+    newNumber = true;
+    lastNumber = undefined;
+}
+document.getElementById('cleanCalc').addEventListener('click', cleanCalc);
+
+const removeLastNumber = () => {
+    display.textContent = display.textContent.slice(0, -1);
+}
+document.getElementById('backspace').addEventListener('click', removeLastNumber);
+
+const changeSignal = () => {
+    newNumber = true;
+    updateDisplay(display.textContent * -1);
+}
+document.getElementById('changeSignal').addEventListener('click', changeSignal);
+
+const existDecimal = () => display.textContent.indexOf(',') !== -1;
+const insertDecimal = () => {
+    if (!existDecimal()) {
+        if (display.textContent.length > 0) {
+            updateDisplay(',');
+        } else {
+            updateDisplay('0,');
+            console.log(existDecimal);
+        }
+    }
+}
+document.getElementById('decimal').addEventListener('click', insertDecimal);
+
+
+const keyboardMap = {
+    '0': 'key0',
+    '1': 'key1',
+    '2': 'key2',
+    '3': 'key3',
+    '4': 'key4',
+    '5': 'key5',
+    '6': 'key6',
+    '7': 'key7',
+    '8': 'key8',
+    '9': 'key9',
+    '/': 'operatorDivision',
+    '*': 'operatorMultiplier',
+    '-': 'operatorMinus',
+    '+': 'operatorPlus',
+    '=': 'equal',
+    'Enter': 'equal',
+    'Backspace': 'backspace',
+    'c': 'cleanDisplay',
+    'Escape': 'cleanCalc',
+    ',': 'decimal'
+}
+const keyboardMaping = (eventAction) => {
+    const keymap = eventAction.key;
+
+    const allowedkey = () => Object.keys(keyboardMap).indexOf(keymap) !== -1;
+    if (allowedkey()) {
+        document.getElementById(keyboardMap[keymap]).click();
+    }
+}
+document.addEventListener('keydown', keyboardMaping);
