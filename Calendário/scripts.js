6f7289a5f984c336646b16f6b44c3490690a04fc
Davi de Godoy Ferreira <davi.ferreira@backlgrs.com.br>
2023-02-15 10:44:25 -0300

    javascript created calendar

---
+++ Calendário/scripts.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const monthIndex = new Date().getMonth();
+
+console.log(monthIndex);
+
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
