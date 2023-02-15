7407ea103bf235b7018e7715b9abeadfd2ad5e6b
Davi de Godoy Ferreira <davi.ferreira@backlgrs.com.br>
2023-02-15 14:31:46 -0300

    complete calendar

--- Calendário/scripts.js
+++ Calendário/scripts.js
@@ -2,7 +2,13 @@
 
 const monthIndex = new Date().getMonth();
 
-console.log(monthIndex);
+const monthElement = document.querySelector('.date h1');
+const fullDateElement = document.querySelector('.date p');
+const daysElement = document.querySelector('.days');
+
+const lastDay = new Date(new Date().getFullYear(), monthIndex + 1, 0).getDate();
+const firstDay = new Date(new Date().getFullYear(), monthIndex, 1).getDay() - 1;
+
 
 const months = [
     "January",
@@ -18,3 +24,21 @@ const months = [
     "November",
     "December"
 ]
+
+monthElement.innerText = months[monthIndex];
+fullDateElement.innerText = new Date().toDateString();
+
+let days = '';
+
+for (let i = firstDay; i > 0; i--) {
+    days += `<div class="empty"></div>`
+}
+
+for (let i = 1; i <= lastDay; i++){
+    if ((i === new Date().getDate())) {
+        days += `<div class="numberDay today">${i}</div>`
+    }
+    days += `<div class="numberDay">${i}</div>`
+}
+
+daysElement.innerHTML = days;
