4b8333a7ec6ef06dd2c444a0c1235c7af31cfffc
Davi de Godoy Ferreira <davigf2002@gmail.com>
2023-02-28 15:07:05 -0300

    uncommenting code

--- src/App.vue
+++ src/App.vue
@@ -11,5 +11,18 @@ import Counter from './components/Counter.vue'
   <Counter />
 </template>
 
-<style scoped>
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  font-family: $fontFamily;
+  letter-spacing: -0.02em;
+}
+
+body {
+  background: $bgMain;
+}
 </style>
--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -12,41 +12,38 @@
     </div>
 </template>
 <script>
-// 'use strict'
+let count = 0;
 
-// let count = 0;
+const counter = document.querySelector('#count');
+const buttons = document.querySelectorAll('.btn');
 
-// const counter = document.querySelector('#count');
-// const buttons = document.querySelectorAll('.btn');
-
-// buttons.forEach(function(btn) {
-//     btn.addEventListener('click', function (e) {
-//         const buttonsClass = e.currentTarget.classList;
-//         if (buttonsClass.contains("decrease")) {
-//             count--;
-//         }
-//         else if(buttonsClass.contains("increase")) {
-//             count++;
-//         }
-//         else {
-//             count = 0;
-//         }
-//         if (count > 0) {
-//             counter.style.color = '#3A8600';
-//         }
-//         if (count < 0) {
-//             counter.style.color = '#CE181E';
-//         }
-//         if (count === 0) {
-//             counter.style.color = '#E5E5E5';
-//         }
-//         counter.innerHTML = count;
-//     })
-// })
+buttons.forEach(function(btn) {
+    btn.addEventListener('click', function (e) {
+        const buttonsClass = e.currentTarget.classList;
+        if (buttonsClass.contains("decrease")) {
+            count--;
+        }
+        else if(buttonsClass.contains("increase")) {
+            count++;
+        }
+        else {
+            count = 0;
+        }
+        if (count > 0) {
+            counter.style.color = '#3A8600';
+        }
+        if (count < 0) {
+            counter.style.color = '#CE181E';
+        }
+        if (count === 0) {
+            counter.style.color = '#E5E5E5';
+        }
+        counter.innerHTML = count;
+    })
+})
 </script>
 <style lang="scss" scoped>
 #appCounter {
-    width: 100vw;
     height: 100vh;
     display: grid;
     place-content: center;
