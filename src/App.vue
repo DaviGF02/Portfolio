8888003ca1ec086a46d035d2b6a716f7a8b59e07
Davi de Godoy Ferreira <davigf2002@gmail.com>
2023-02-28 13:11:49 -0300

    changing css to scss

--- src/App.vue
+++ src/App.vue
@@ -7,22 +7,9 @@ import Counter from './components/Counter.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
   <Calculator />
   <Calendar />
   <Counter />
-  <RouterView />
 </template>
 
 <style scoped>
