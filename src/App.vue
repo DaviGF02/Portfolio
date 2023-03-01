941fddfccff6299669839cf1dd07d49d9439f8e5
Davi de Godoy Ferreira <davi.ferreira@backlgrs.com.br>
2023-03-01 19:06:57 -0300

    refactor appVue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <script setup>
 import Calculator from './components/Calculator.vue'
-import Calendar from './components/Calendar.vue'
 import Counter from './components/Counter.vue'
 </script>
 
 <template>
   <Calculator />
-  <Calendar />
   <Counter />
 </template>
 
