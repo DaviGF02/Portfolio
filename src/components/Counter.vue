4bb3beaa75cfb696d142181d92f14c22fa0a7bdf
Davi de Godoy Ferreira <davi.ferreira@backlgrs.com.br>
2023-03-01 16:43:37 -0300

    starting javascript refactoring

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -11,37 +11,13 @@
     </div>
   </div>
 </template>
+
 <script>
-// let count = 0;
-
-// const counter = document.querySelector('#count');
-// const buttons = document.querySelectorAll('.btn');
-
-// buttons.forEach(function(btn) {
-//     btn.addEventListener('click', function (e) {
-//         const buttonsClass = e.currentTarget.classList;
-//         if (buttonsClass.contains("decrease")) {
-//             count--;
-//         }
-//         else if(buttonsClass.contains("increase")) {
-//             count++;
-//         }
-//         else {
-//             count = 0;
-//         }
-//         if (count > 0) {
-//             counter.style.color = '#3A8600';
-//         }
-//         if (count < 0) {
-//             counter.style.color = '#CE181E';
-//         }
-//         if (count === 0) {
-//             counter.style.color = '#E5E5E5';
-//         }
-//         counter.innerHTML = count;
-//     })
-// })
+export default {
+  name: 'Counter',
+}
 </script>
+
 <style lang="scss" scoped>
 #appCounter {
   height: 100vh;
