61220f9fc0f10c25d7d1d0c78e2993324f3edad0
Davi de Godoy Ferreira <davigf2002@gmail.com>
2023-03-09 19:57:21 -0300

    animate navMenu

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -1,22 +1,86 @@
 <template>
-    <div class="menu">
-        <nav>
-            <RouterLink class="navLink" to="/Counter"><i class="fa-solid fa-arrow-up-9-1"></i> Contador</RouterLink>
-            <RouterLink class="navLink" to="/Dashboard"><i class="fa-solid fa-chart-line"></i> Dashboard</RouterLink>
-        </nav>
+  <div class="menu">
+    <i @click="showMenu" class="fa-solid fa-bars"></i>
+    <div class="menuLinks" :class="{ active: isActive}">
+      <RouterLink @click="showMenu" class="navLink" to="/Counter"
+        ><i class="fa-solid fa-arrow-up-9-1"></i> Contador</RouterLink
+      >
+      <RouterLink @click="showMenu" class="navLink" to="/Dashboard"
+        ><i class="fa-solid fa-chart-line"></i> Dashboard</RouterLink
+      >
     </div>
+    <div @click="showMenu" class="bg__menuLinks" :class="{bg__active: isActive}"></div>
+  </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      isActive: false,
+    }
+  },
+  methods: {
+    showMenu() {
+      this.isActive = !this.isActive
+    }
+  }
+}
+</script>
 <style lang="scss" scoped>
 .menu {
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    padding: 2rem 0 0 0;
+  .menuLinks {
+    position: fixed;
+    height: 100vh;
+    width: 30rem;
+    background: $bgDark;
+    display: none;
+    padding: 2rem;
+    opacity: 0;
+    z-index: 1001;
     .navLink {
-        color: $envelopeWhite;
-        text-decoration: none;
-        font-size: 2rem;
-        padding: 0rem 2rem;
+      color: $envelopeWhite;
+      text-decoration: none;
+      font-size: 2rem;
     }
+  }
+  .bg__menuLinks {
+    position: fixed;
+    background: rgba($color: $black, $alpha: 0.25);
+    width: 100vw;
+    height: 100vh;
+    z-index: 1000;
+    opacity: 0;
+    display: none;
+  }
+  .active {
+    animation: showMenuAnimation .5s;
+    opacity: 1;
+    display: flex;
+    flex-direction: column;
+  }
+  .bg__active {
+    animation: showMenuAnimation .5s;
+    opacity: 1;
+    display: block;
+  }
+  @keyframes showMenuAnimation {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+  .fa-bars {
+    color: $envelopeWhite;
+    position: fixed;
+    top: 3rem;
+    left: 3rem;
+    font-size: 3rem;
+    background-color: $bgDark;
+    padding: 1rem;
+    border-radius: 1rem;
+    @include buttonShadow;
+  }
 }
 </style>
