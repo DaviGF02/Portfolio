6f4a7568f2f5d2579576ba15a4568e016aee5ed5
Davi de Godoy Ferreira <davigf2002@gmail.com>
2023-03-10 18:42:01 -0300

    create chart example and add chart library

---
+++ src/components/ChartJs/ModelChart.vue
@@ -0,0 +1,55 @@
+<template>
+    <canvas id="myChart" width="400" height="400"></canvas>
+</template>
+<script>
+import Chart from 'chart.js/auto'
+export default {
+  name: 'appModelChart',
+  mounted() {
+    const data = {
+      labels: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'],
+      datasets: [
+        {
+          label: 'My First Dataset',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 205, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(201, 203, 207, 0.2)'
+          ],
+          borderColor: [
+            'rgb(255, 99, 132)',
+            'rgb(255, 159, 64)',
+            'rgb(255, 205, 86)',
+            'rgb(75, 192, 192)',
+            'rgb(54, 162, 235)',
+            'rgb(153, 102, 255)',
+            'rgb(201, 203, 207)'
+          ],
+          borderWidth: 1
+        }
+      ]
+    }
+
+    const config = {
+      type: 'bar',
+      data: data,
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    }
+
+    const ctx = document.getElementById('myChart')
+    const myChart = new Chart(ctx, config)
+    myChart
+  }
+}
+</script>
