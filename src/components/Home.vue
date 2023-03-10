93eac6a16c4e5fc2c697ea7ec1554263253eadfe
Davi de Godoy Ferreira <davigf2002@gmail.com>
2023-03-10 15:28:25 -0300

    delete vue component 3d and add cientos library

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,9 @@
 <template>
   <div id="appHome">
-    <Component3d />
   </div>
 </template>
 
 <script>
-import Component3d from './TresJs/FirstComponent3d.vue'
-export default {
-  components: {
-    Component3d,
-  }
-}
 </script>
 
 <style lang="scss" scoped>
