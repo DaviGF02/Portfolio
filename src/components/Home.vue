fb76acae79fed256d547a880351c9a77d1a7cc12
Davi de Godoy Ferreira <davigf2002@gmail.com>
2023-03-10 02:03:07 -0300

    my first 3d element with tresJs

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,16 @@
 <template>
   <div id="appHome">
+    <Component3d />
   </div>
 </template>
 
 <script>
+import Component3d from './TresJs/FirstComponent3d.vue'
+export default {
+  components: {
+    Component3d,
+  }
+}
 </script>
 
 <style lang="scss" scoped>
---
+++ src/components/TresJs/FirstComponent3d.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+<template>
+  <TresCanvas window-size>
+    <TresPerspectiveCamera :args="[75, 1, 0.1, 1000]"
+    :position="[3,1,4]"
+    />
+    <TresScene>
+      <TresMesh :position="[0, 2, 0]">
+        <TresBoxGeometry :args="[1.5,3,0.5]" />
+        <TresMeshNormalMaterial/>
+      </TresMesh>
+    </TresScene>
+    <TresAxesHelper />
+  </TresCanvas>
+</template>
