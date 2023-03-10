203451e061efabb2d0aa74975afba690aa8f81f9
Davi de Godoy Ferreira <davigf2002@gmail.com>
2023-03-10 00:50:22 -0300

    add tresjs library

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,27 +1,9 @@
 <template>
   <div id="appHome">
-    <canvas ref="canvas"></canvas>
   </div>
 </template>
 
 <script>
-import * as THREE from 'three';
-export default {
-  data() {
-    return {
-      scene: null
-    }
-  },
-  mounted() {
-    this.scene = new THREE.Scene();
-    this.renderer = new THREE.WebGLRenderer({
-        canvas: this.$refs.canvas
-    });
-
-    this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-    this.renderer.render(this.scene, this.camera)
-  }
-}
 </script>
 
 <style lang="scss" scoped>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import router from './router'
 import App from './App.vue'
+import Tres from '@tresjs/core'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Tres)
 
 app.mount('#app')
